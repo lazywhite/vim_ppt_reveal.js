bc4b7df81edfcfb37e5fdcbdc5739ef59f912dcc
white <346816483@qq.com>
2016-08-14 17:40:59 +0800

    initial commit

---
+++ config/index.js
@@ -0,0 +1,14 @@
+var config = {
+	// store default list of presentations
+	presentations : {
+		'vim' : { // demo presentation
+			id: 'vim',  // id for each presentation, currently same as the url
+			title : 'Demo Presentation',
+			indexh : 0,  // initial slide horizontal index
+			indexv : 0,  // initial slide veriticlal index
+		},
+	}
+
+};
+
+exports.config = config;
